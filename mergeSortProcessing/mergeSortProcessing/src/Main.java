2f3a22e1aab255ae48d72d40eab26025efc13468
Frank Baethge <baethge@kabelmail.de>
2014-12-15 19:52:49 +0100

    Erweiterung für Quicksort - noch unvollständig

--- mergeSortProcessing/mergeSortProcessing/src/Main.java
+++ mergeSortProcessing/mergeSortProcessing/src/Main.java
@@ -4,8 +4,8 @@ import java.util.Random;
 
 public class Main extends PApplet{
 
-	protected final Integer länge = 30; // Anzahl der Feldelemente
-	protected final Integer größe = 20; // Kantenlänge eines Quadrates
+	protected final Integer länge = 50; // Anzahl der Feldelemente
+	protected final Integer größe = 9; // Kantenlänge eines Quadrates
 	protected  Integer zeile = 0;   // Zeile, in der die jeweilige Feldbelegung angezeigt wird;
 
 	public int[] feld = new int[länge];
@@ -20,7 +20,7 @@ public class Main extends PApplet{
 		fülleFeldMitZufallszahlen(255);
 		ausgeben2Str(feld, 0);
 		mergeSort(feld, 0);
-		save("mergeSort08.png");
+		save("mergeSort09.png");
 	}
 
 	public void draw() {
@@ -46,7 +46,7 @@ public class Main extends PApplet{
 		int i,j, merke;
 		// fülle mit Grauwerten
 		for (i = 0; i < feld.length; i++) {
-			feld[i] = i*zahlenBis/länge;
+			feld[i] = i*5 + (255 - länge * 5);
 		}
 		// permutiere zufällig
 		Random rand = new Random();
@@ -103,4 +103,41 @@ public class Main extends PApplet{
 		System.arraycopy(zweite, z, ergebnis, f, zweite.length - z);
 		ausgeben2Str(ergebnis, offset);
 	}
+
+/*
+*
+    public static void sort(Adresse[] a) {
+        start = System.nanoTime();
+        quicksort(a, 0, (a.length - 1));
+        ende = System.nanoTime();
+    }
+
+    public static void quicksort(Adresse[] a, int links, int rechts) {
+        if ((rechts - links) <= 1) {
+            return;
+        }
+        Adresse p = a[links];
+        //System.out.println("p=" + p + " links: " + links + " rechts: " + rechts);
+        int z1 = links;
+        int z2 = rechts;
+        do {
+            if (a[z1].compareTo(p) > 0) {
+                if (a[z2].compareTo(p) <= 0) {
+                    Adresse zwischenwert = a[z1];
+                    a[z1] = a[z2];
+                    a[z2] = zwischenwert;
+                } else {
+                    z2--;
+                }
+            } else {
+                z1++;
+            }
+        } while (z1 < z2);
+        Adresse zwischenwert = a[z1 - 1];
+        a[z1 - 1] = a[links];
+        a[links] = zwischenwert;
+        quicksort(a, links, (z1 - 1));
+        quicksort(a, z2, rechts);
+    }
+* */
 }
